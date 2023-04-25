5005290a21d9c23a31bce877910321a138d5010a
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-26 00:05:12 +0700

    Enable webflux

---
+++ hantsy/hantsy-vanilla-netty/src/main/java/io/github/bluething/playground/spring/reactive/hantsy/vanilla/netty/WebConfig.java
@@ -0,0 +1,11 @@
+package io.github.bluething.playground.spring.reactive.hantsy.vanilla.netty;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@ComponentScan
+@EnableWebFlux
+public class WebConfig {
+}
