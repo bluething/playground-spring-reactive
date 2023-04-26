4539847c29b8a4eec97be6647a20642b36ddd588
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-26 10:52:57 +0700

    Init

---
+++ hantsy/hantsy-router-function/src/test/java/io/github/bluething/playground/spring/reactive/hantsy/router/function/HantsyRouterFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.reactive.hantsy.router.function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HantsyRouterFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
