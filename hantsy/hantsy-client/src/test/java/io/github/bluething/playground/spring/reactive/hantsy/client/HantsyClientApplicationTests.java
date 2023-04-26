d913db740b4b13d79d664258a1d35f78b118e8c4
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-26 14:43:45 +0700

    Init

---
+++ hantsy/hantsy-client/src/test/java/io/github/bluething/playground/spring/reactive/hantsy/client/HantsyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.reactive.hantsy.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HantsyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
