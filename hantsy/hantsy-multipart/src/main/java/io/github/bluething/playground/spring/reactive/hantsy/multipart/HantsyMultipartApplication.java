9e8f4aea2ced126302e1aab502518a0789b80968
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-26 15:38:55 +0700

    Init

---
+++ hantsy/hantsy-multipart/src/main/java/io/github/bluething/playground/spring/reactive/hantsy/multipart/HantsyMultipartApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.reactive.hantsy.multipart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HantsyMultipartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HantsyMultipartApplication.class, args);
+	}
+
+}
---
+++ hantsy/hantsy-multipart/src/test/java/io/github/bluething/playground/spring/reactive/hantsy/multipart/HantsyMultipartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.reactive.hantsy.multipart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HantsyMultipartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
