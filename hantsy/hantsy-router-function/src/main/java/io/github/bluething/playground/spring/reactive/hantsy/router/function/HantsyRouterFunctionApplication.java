58bc859dffad3eb398d46694da28055b6d856bbb
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-26 10:53:21 +0700

    The main

---
+++ hantsy/hantsy-router-function/src/main/java/io/github/bluething/playground/spring/reactive/hantsy/router/function/HantsyRouterFunctionApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.reactive.hantsy.router.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HantsyRouterFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HantsyRouterFunctionApplication.class, args);
+	}
+
+}
